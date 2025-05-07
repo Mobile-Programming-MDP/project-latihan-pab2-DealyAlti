27c27e6505bdfab76bb801756b41d4874cbcdd44
Dealy <dealyalti_2226240037@mhs.mdp.ac.id>
2025-05-07 18:35:19 +0700

    update add post

--- fasum/lib/screens/home_screen.dart
+++ fasum/lib/screens/home_screen.dart
@@ -4,13 +4,37 @@ import 'package:fasum/screens/add_post_screen.dart';
 import 'package:fasum/screens/detail_screen.dart';
 import 'package:fasum/screens/sign_in_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String? selectedCategory;
+  List<String> categories = [
+    'Jalan Rusak',
+    'Marka Pudar',
+    'Lampu Mati',
+    'Trotoar Rusak',
+    'Rambu Rusak',
+    'Jembatan Rusak',
+    'Sampah Menumpuk',
+    'Saluran Tersumbat',
+    'Sungai Tercemar',
+    'Sampah Sungai',
+    'Pohon Tumbang',
+    'Taman Rusak',
+    'Fasilitas Rusak',
+    'Pipa Bocor',
+    'Vandalisme',
+    'Banjir',
+    'Lainnya',
+  ];
   String formatTime(DateTime dateTime) {
     final now = DateTime.now();
     final diff = now.difference(dateTime);
@@ -25,14 +49,69 @@ class HomeScreen extends StatelessWidget {
     }
   }
 
-  Future<void> signOut(BuildContext context) async {
+  Future<void> signOut() async {
     await FirebaseAuth.instance.signOut();
-
+    if (!mounted) return;
     Navigator.of(context).pushReplacement(
       MaterialPageRoute(
         builder: (context) => SignInScreen()
       )
     );
+  }
+
+  void _showCategoryFilter() async {
+    final result = await showModalBottomSheet<String?>(
+      context: context,
+      isScrollControlled: true,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(24)),
+      ),
+      builder: (context) {
+        return SafeArea(
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height * 0.75,
+            child: ListView(
+              padding: const EdgeInsets.only(bottom: 24),
+              children: [
+                ListTile(
+                  leading: const Icon(Icons.clear),
+                  title: const Text('Semua Kategori'),
+                  onTap: () => Navigator.pop(
+                    context,
+                    null,
+                  ), // Null untuk memilih semua kategori
+                ),
+                const Divider(),
+                ...categories.map(
+                  (category) => ListTile(
+                    title: Text(category),
+                    trailing: selectedCategory == category
+                        ? Icon(
+                            Icons.check,
+                            color: Theme.of(context).colorScheme.primary,
+                          )
+                        : null,
+                    onTap: () => Navigator.pop(context, category),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+    if (result != null) {
+      setState(() {
+        selectedCategory =
+            result; // Set kategori yang dipilih atau null untuk Semua Kategori
+      });
+    } else {
+      // Jika result adalah null, berarti memilih Semua Kategori
+      setState(() {
+        selectedCategory =
+            null; // Reset ke null untuk menampilkan semua kategori
+      });
+    }
   }
 
   @override
@@ -43,8 +122,12 @@ class HomeScreen extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         actions: [
           IconButton(
+            onPressed: _showCategoryFilter, 
+            icon: const Icon(Icons.filter_list)
+          ),
+          IconButton(
             onPressed: (){
-              signOut(context);
+              signOut();
             },
             icon: const Icon(Icons.logout),
           ),
@@ -56,36 +139,57 @@ class HomeScreen extends StatelessWidget {
           if (!snapshot.hasData)
             return Center(child: CircularProgressIndicator());
 
-            final posts = snapshot.data!.docs;
+            final posts = snapshot.data!.docs.where((doc){
+              final data = doc.data();
+              final category = data['category'] ?? 'Lainnya';
+              return selectedCategory==null || selectedCategory==category;
+            }).toList();
+            if(posts.isEmpty){
+              return const Center(
+                child: Text("Tidak ada laporan untuk kategori ini!"),
+              );
+            }
             return ListView.builder(
             itemCount: posts.length,
             itemBuilder: (context, index) {
               final data = posts[index].data();
               final imageBase64 = data['image'];
               final description = data['description'];
-              final createdAtStr = data['createdAt'];
               final fullName = data['fullName'] ?? 'Anonim';
               final latitude = data['latitude'];
               final longitude = data['longitude'];
               final category = data['category'] ?? 'Lainnya';
  
-              //parse ke DateTime
-              final createdAt = DateTime.parse(createdAtStr);
-              String heroTag = 'fasum-image-${createdAt.millisecondsSinceEpoch}';
+              // parse ke DateTime
+              DateTime createdAt;
+              final createdAtValue = data['createdAt'];
+
+              if (createdAtValue is Timestamp) {
+                createdAt = createdAtValue.toDate();
+              } else if (createdAtValue is String) {
+                createdAt = DateTime.parse(createdAtValue);
+              } else {
+                createdAt = DateTime.now();
+              }
+
+              String heroTag = 
+                'fasum-image-${createdAt.millisecondsSinceEpoch}';
+
+
               return InkWell(
-                onTap: (){
+                onTap: () {
                   Navigator.push(
-                    context, 
+                    context,
                     MaterialPageRoute(
                       builder: (context) => DetailScreen(
-                          imageBase64: imageBase64,
-                          description: description,
-                          createdAt: createdAt,
-                          fullName: fullName,
-                          latitude: 0.0,
-                          longitude: 0.0,
-                          category: "Jalan Rusak",
-                          heroTag: heroTag),
+                        imageBase64: imageBase64, 
+                        description: description, 
+                        createdAt: createdAt, 
+                        fullName: fullName, 
+                        latitude: latitude, 
+                        longitude: longitude, 
+                        category: category, 
+                        heroTag: heroTag),
                     ),
                   );
                 },
